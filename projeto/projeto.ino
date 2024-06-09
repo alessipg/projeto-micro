9d9153cb8b674e53c28e66edc4342090d82d6e7c
alessipg <gabriel.alessip@gmail.com>
2024-06-09 12:02:53 -0300

    removi a servo lib

--- projeto/projeto.ino
+++ projeto/projeto.ino
@@ -1,40 +1,52 @@
-#include <Servo.h>
-
 #define botao 2
 #define led 3
 #define servo 4
-Servo myservo;
+#define degree 90
+
 void setup() {
   pinMode(led, OUTPUT);
   pinMode(botao, INPUT);
-  myservo.attach(servo);
+  pinMode(servo, OUTPUT);
 }
+
 bool estadoBotao;
-bool estadoAnteriorBotao;
+bool estadoAnteriorBotao = LOW;
 bool cancela = false;
-int pos;
+byte pos;
+
 void loop() {
-  estadoBotao=digitalRead(botao);
-  if(estadoBotao != estadoAnteriorBotao && estadoBotao == 1){
-  	digitalWrite(led, !digitalRead(led));
-    switchServo();
-    delay(30);
+  estadoBotao = digitalRead(botao);
+
+  // Verifica se o botão foi pressionado (borda de subida)
+  if (estadoBotao != estadoAnteriorBotao && estadoBotao == HIGH) {
+    cancela = !cancela;
+    digitalWrite(led, cancela);
+    switchServo(cancela);
   }
   
-  estadoAnteriorBotao=estadoBotao;
+  estadoAnteriorBotao = estadoBotao;
 }
 
-void switchServo(){
-
-if (digitalRead(led)) {
-    for (pos = 0; pos <= 90; pos += 1) {
-      myservo.write(pos);
-      delay(15);
+void switchServo(bool estado) {
+  if (estado) {
+    // Move de 0 a 90 graus
+    for (pos = 0; pos < degree; pos++) {
+      setAngulo(pos, servo);
+      delay(15); // Ajuste este valor para controlar a velocidade do movimento
     }
-  }else{
-    for (pos = 90; pos >= 0; pos -= 1) {
-      myservo.write(pos);
-      delay(15);
+  } else {
+    // Move de 90 a 0 graus
+    for (pos = degree; pos > 0; pos--) {
+      setAngulo(pos, servo);
+      delay(15); // Ajuste este valor para controlar a velocidade do movimento
     }
   }
+}
+
+void setAngulo(int angulo, int pin) {
+  int val = map(angulo, 0, 180, 544, 2400); // Ajuste de valor para controle do servo
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(val);
+  digitalWrite(pin, LOW);
+  delay(20); // Tempo necessário para o servo completar o movimento
 }
